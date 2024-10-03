cac58fa8f7cf8dc6123f5e6110de0637075d7054
arecouz <richardcouzens96@gmail.com>
2024-10-03 13:00:23 +0100

    Exercise 13.3 (implementing node and sequalize)

---
+++ index.js
@@ -0,0 +1,22 @@
+// tunnel first: flyctl proxy 5432 -a fso-blogs-relational
+
+require('dotenv').config();
+const { Sequelize, QueryTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const main = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    const blogs = await sequelize.query('SELECT * FROM blogs', {
+      type: QueryTypes.SELECT,
+    });
+    console.log(blogs);
+    sequelize.close();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+main();
