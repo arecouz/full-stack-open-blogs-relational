80149f5b2aa7a98c8c9c211b11bbb25260978674
arecouz <richardcouzens96@gmail.com>
2024-10-04 13:40:45 +0100

    initial web functionality

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 // tunnel first: flyctl proxy 5432 -a fso-blogs-relational
 
 require('dotenv').config();
-const { Sequelize, QueryTypes } = require('sequelize');
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize');
+const express = require('express')
+const app = express ()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    const blogs = await sequelize.query('SELECT * FROM blogs', {
-      type: QueryTypes.SELECT,
-    });
-    console.log(blogs);
-    sequelize.close();
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false
+    }
   }
-};
+}
+);
 
-main();
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER, 
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.STRING
+  },
+  url: {
+    type: DataTypes.STRING,
+    require: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    require: true
+  },
+  likes: {
+    types: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+})
+
+Blog.sync()
+
+
+app.use(express.json)
+
+app.get('/api/blogs', async(req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs) 
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`)
+})
